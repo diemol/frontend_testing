15387886ff2bbb7fdece5b9a3760bd13ef628e50
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-05-06 13:11:55 +0200

    Adding example to run tests in parallel also with different browsers.

---
+++ webdriver-examples-js-parallel/threads-and-browsers/test/user.js
@@ -0,0 +1,49 @@
+// Getting the Chai expect library for assertions
+var expect = require('chai').expect;
+
+// Create a new instance of WebDriver and other helpful methods from WebDriver
+var webDriver = require('selenium-webdriver');
+var By = webDriver.By;
+var until = webDriver.until;
+
+var createDriver = require('./base').createDriver;
+var testBrowsers = require('./base').testBrowsers;
+
+describe('User Test', function() {
+    this.timeout(60000);
+
+    testBrowsers().forEach(function(testBrowser) {
+        it('Register user should leave new user logged in. ' + testBrowser.browser + ", " + testBrowser.platform,
+            function(done) {
+
+            var driver = createDriver(this._runnable.title, testBrowser.browser, testBrowser.platform);
+
+            // Go to the register page
+            driver.get("http://phptravels.net/register");
+
+            // Filling up the form to Sign Up
+            driver.findElement(By.name("firstname")).sendKeys("John");
+            driver.findElement(By.name("lastname")).sendKeys("Doe");
+            driver.findElement(By.name("phone")).sendKeys("+1223456789");
+            var randomEmail = Math.random().toString(36).substring(7) + "@example.com";
+            driver.findElement(By.name("email")).sendKeys(randomEmail);
+            var randomPassword = Math.random().toString(36).substring(10);
+            driver.findElement(By.name("password")).sendKeys(randomPassword);
+            driver.findElement(By.name("confirmpassword")).sendKeys(randomPassword);
+            //Click on "Sign Up"
+            var signUpButton = driver.findElement(By.css("button.signupbtn.btn_full.btn.btn-primary.btn-block.btn-lg"));
+            signUpButton.click();
+            // After Signing Up, we assert that we are logged in by checking the name displayed on the user page
+            var welcomeMessage = driver.wait(until.elementLocated(By.css("h3.RTL"), 10000));
+            var expectedWelcomeMessage = "Hi, John Doe";
+            welcomeMessage.getText().then(function(welcomeText) {
+                expect(welcomeText).to.equal(expectedWelcomeMessage);
+            });
+
+            // Quitting the browser and invoking the callback function to tell Mocha that we are done
+            driver.quit().then(done);
+        });
+    });
+
+});
+
