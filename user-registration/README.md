ee1dff34735d7596bf2ca16d152ec50e3d8eb138
Diego Fernando Molina Bocanegra <diego.fernando.molina.bocanegra@zalando.de>
2016-06-01 16:20:44 +0200

    Isolating user registration example in its own folder.

---
+++ user-registration/README.md
@@ -0,0 +1,18 @@
+### Simple user registration example using WebDriver in Java and JavaScript
+
+This is a simple example where a user gets registered on a website.
+
+Implemented in [Java with TestNG](https://github.com/diemol/frontend_testing/tree/master/user-registration/java-testng)
+and [JavaScript with Mocha and Chai](https://github.com/diemol/frontend_testing/tree/master/user-registration/js-mocha-chai).
+
+##### [docker-selenium](https://github.com/elgalu/docker-selenium) is used to run the tests
+This means:
+* You need to have [docker](https://www.docker.com/) installed. Here are the instructions for [Mac](https://docs.docker.com/mac/), [Linux](https://docs.docker.com/linux/) and [Windows](https://docs.docker.com/windows/).
+* After installing docker, and before running the tests, just run this command to start the docker-selenium container:
+
+  ```
+  $ docker run --rm -ti --name=grid -p 4444:24444 -p 5920:25900 -v /dev/shm:/dev/shm -p 6080:26080 -e NOVNC=true -e VNC_PASSWORD=hola elgalu/selenium:2.53.0g
+  ```
+* When the container starts, you can see the Selenium Grid running at [http://localhost:4444/grid/console](http://localhost:4444/grid/console). **Attention**: If you are running Mac, `localhost` may not work, use `docker-machine ip default` to find out the correct IP for you.
+
+* If you want to see the browsers while the test is running, you can access the container with VNC through [http://localhost:6080/vnc.html](http://localhost:6080/vnc.html). The password is `hola`, it was set in the docker command. As in the previous point, check your docker IP.
--- user-registration/java-testng/src/test/java/UserRegistrationTest.java
+++ user-registration/java-testng/src/test/java/UserRegistrationTest.java
@@ -1,17 +1,48 @@
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class UserTest extends BaseTest {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UserRegistrationTest {
+
+    // Setting the url for the WebDriver
+    public static final String DOCKER_MACHINE_HOST = System.getenv("DOCKER_MACHINE_HOST");
+    public static final String URL = String.format("http://%s:4444//wd/hub", DOCKER_MACHINE_HOST);
+    public WebDriver webDriver;
+
+    @BeforeMethod
+    public void startWebDriver() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
+
+        webDriver = new RemoteWebDriver(new URL(URL), desiredCapabilities);
+
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void quitBrowser() {
+        webDriver.quit();
+    }
 
     @Test
     public void registerUserShouldLeaveNewUserLoggedIn() {
         // Go to the register page
         webDriver.get("http://phptravels.net/register");
+
         // Filling up the form to Sign Up
         webDriver.findElement(By.name("firstname")).sendKeys("John");
         webDriver.findElement(By.name("lastname")).sendKeys("Doe");
@@ -21,9 +52,11 @@ public class UserTest extends BaseTest {
         String randomPassword = RandomStringUtils.randomAlphanumeric(10);
         webDriver.findElement(By.name("password")).sendKeys(randomPassword);
         webDriver.findElement(By.name("confirmpassword")).sendKeys(randomPassword);
+
         //Click on "Sign Up"
-        WebElement signUpButton = webDriver.findElement(By.cssSelector("button.signupbtn.btn_full.btn.btn-primary.btn-block.btn-lg"));
+        WebElement signUpButton = webDriver.findElement(By.cssSelector("button.signupbtn.btn_full.btn.btn-action.btn-block.btn-lg"));
         signUpButton.click();
+
         // After Signing Up, we assert that we are logged in by checking the name displayed on the user page
         WebDriverWait wait = new WebDriverWait(webDriver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3.RTL")));
@@ -31,4 +64,6 @@ public class UserTest extends BaseTest {
         String expectedWelcomeMessage = "Hi, John Doe";
         Assert.assertEquals(welcomeMessage.getText(), expectedWelcomeMessage, "Welcome message was not the expected one");
     }
+
+
 }
