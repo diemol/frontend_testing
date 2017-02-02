c1dab6161f3016863e3f33a5d098dc36bbfa35cb
Diego Molina <diemol@gmail.com>
2017-02-02 12:25:31 +0100

    Adjusting parallel complete example to use Zalenium

--- running-in-parallel/java-testng/complete/src/test/java/BaseTest.java
+++ running-in-parallel/java-testng/complete/src/test/java/BaseTest.java
@@ -14,11 +14,7 @@ import java.net.URL;
 
 public class BaseTest {
 
-    // SauceLabs setup variables
-    public static final String userName = System.getenv("SAUCE_USERNAME");
-    public static final String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-    public static final String SAUCE_LABS_URL = String.format("http://%s:%s@ondemand.saucelabs.com:80/wd/hub",
-            userName, accessKey);
+    private static final String SELENIUM_GRID_URL = "http://localhost:4444/wd/hub";
 
     // We need a thread safe environment to handle the webDriver variable in each thread separately
     private ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
@@ -28,10 +24,7 @@ public class BaseTest {
     public static Object[][] browsersAndPlatformsProvider() {
         return new Object[][] {
                 new Object[]{BrowserType.CHROME, Platform.LINUX},
-                new Object[]{BrowserType.FIREFOX, Platform.LINUX},
-                new Object[]{BrowserType.CHROME, Platform.WIN8},
-                new Object[]{BrowserType.FIREFOX, Platform.WIN8},
-                new Object[]{BrowserType.SAFARI, Platform.EL_CAPITAN}
+                new Object[]{BrowserType.FIREFOX, Platform.LINUX}
         };
     }
 
@@ -44,7 +37,7 @@ public class BaseTest {
         desiredCapabilities.setCapability(CapabilityType.PLATFORM, platform);
         desiredCapabilities.setCapability("name", method.getName());
 
-        webDriver.set(new RemoteWebDriver(new URL(SAUCE_LABS_URL), desiredCapabilities));
+        webDriver.set(new RemoteWebDriver(new URL(SELENIUM_GRID_URL), desiredCapabilities));
 
         webDriver.get().manage().window().maximize();
     }
@@ -55,6 +48,7 @@ public class BaseTest {
     }
 
     // Returns the webDriver for the current thread
+    @SuppressWarnings("WeakerAccess")
     public WebDriver getWebDriver() {
         return webDriver.get();
     }
