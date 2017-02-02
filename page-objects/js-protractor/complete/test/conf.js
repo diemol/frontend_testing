f6c41c4ca1b329cc16f32a3a41a9212918a19500
Leo Gallucci <leo.gallucci@zalando.de>
2017-02-02 17:08:46 +0100

    We can use latest jasmine2

--- page-objects/js-protractor/complete/test/conf.js
+++ page-objects/js-protractor/complete/test/conf.js
@@ -1,6 +1,6 @@
 // conf.js
 exports.config = {
-    framework: 'jasmine',
+    framework: 'jasmine2',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['add-item-to-shopping-cart.js'],
     onPrepare: function(){
