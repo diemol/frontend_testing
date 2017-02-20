20595632490a060121353eef3b842fd21beb3379
Diego Molina <diemol@gmail.com>
2017-02-20 13:37:57 +0100

    Updating locators for initial version of the tests.

--- page-objects/js-protractor/initial/pages/product-detail-page.js
+++ page-objects/js-protractor/initial/pages/product-detail-page.js
@@ -1,14 +1,13 @@
 ProductDetailPage = function ArticleDetailPage() {
     this.productContent = element(by.className('z-vegas-ui_article-brand-info_content'));
-    this.productInfoBrand = this.productContent.all(by.css('.z-vegas-ui_text.z-vegas-ui_text-standard'));
 };
 
 ProductDetailPage.prototype.getProductBrand = function() {
-    return this.productInfoBrand.first().getText();
+    return this.productContent.element(by.css('.z-vegas-ui_text.z-vegas-ui_text-vegas-detail-title')).getText();
 };
 
 ProductDetailPage.prototype.getProductName = function() {
-    return this.productInfoBrand.last().getText();
+    return this.productContent.element(by.css('.z-vegas-ui_text.z-vegas-ui_text-vegas-body')).getText();
 };
 
 module.exports = ProductDetailPage;
