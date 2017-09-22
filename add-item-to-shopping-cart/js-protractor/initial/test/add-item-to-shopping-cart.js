a28a2c255a2d3ed1c24479111b758fa420d1d9e6
Diego Molina <diemol@gmail.com>
2017-09-22 12:41:07 +0200

    Fixing initial example

--- add-item-to-shopping-cart/js-protractor/initial/test/add-item-to-shopping-cart.js
+++ add-item-to-shopping-cart/js-protractor/initial/test/add-item-to-shopping-cart.js
@@ -6,25 +6,17 @@ describe('Add item to Shopping Cart', function() {
 
     it('Load Zalando home page', function() {
         browser.driver.manage().window().maximize();
-        browser.get('https://www.zalando.de/');
-        expect(browser.getTitle()).toEqual('Schuhe & Mode online kaufen | ZALANDO Online Shop');
+        browser.get('https://www.zalando.de/mosaic-mosaic-catalog');
     });
 
     it('Type Nike in the search field', function() {
-        element(by.id('searchContent')).sendKeys('Nike');
+        element(by.id('searchContent')).sendKeys('Adidas');
         element(by.id('searchContent')).submit();
     });
 
     it('Click on the first item', function() {
-        var firstItem = element.all(by.className('catalogArticlesList_item'));
-        firstItem.isPresent().then(function (isPresent) {
-            if (isPresent) {
-                firstItem.first().click();
-            } else {
-                firstItem = element.all(by.css('z-grid[class=z-nvg-cognac_articles]')).first();
-                firstItem.click();
-            }
-        });
+        var firstItem = element.all(by.css('z-grid[class=z-nvg-cognac_articles]')).first();
+        firstItem.click();
     });
 });
 
